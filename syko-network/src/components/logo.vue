38636078640168596e6a776e88e789a7675a88bd
SyKo <timjdcook@gmail.com>
2021-05-28 00:09:17 -0400

    push

---
+++ syko-network/src/components/logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <img
+    width="30"
+    height="30"
+    class="d-inline-block align-top"
+    alt="SyKoNetwork Logo"
+    src="@/assets/pack_icon_alpha.png"
+  />
+</template>
+
+<script>
+export default {
+  name: "Logo",
+};
+</script>
