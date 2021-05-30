d47d9cee7521f6b7ece66872db8e75c96502f21f
SyKo <timjdcook@gmail.com>
2021-05-30 16:52:49 -0400

    updt push

--- syko-network/deploy.sh
+++ syko-network/deploy.sh
@@ -20,7 +20,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:SyKoNetwork/SyKoNetwork.github.io.git main:gh-pages
+#git push -f git@github.com:SyKoNetwork/SyKoNetwork.github.io.git main:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 #git push -f git@github.com:SyKoNetwork/sykonetwork.git main:gh-pages
